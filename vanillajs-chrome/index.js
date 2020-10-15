18ac9443a4b0cf0073f8af60d3e62ca51dbdb404
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-15 20:40:24 +0900

    Update index.js

--- vanillajs-chrome/index.js
+++ vanillajs-chrome/index.js
@@ -2,3 +2,12 @@ const title = document.querySelector("#title");
 title.innerHTML="Hi! From JS";
 title.style.color = "red";
 document.title = 'I own you now';
+
+function handleResize(){
+    console.log("I have been resized.")
+}
+
+/*window.addEventLister("resize", handleResize())로 입력한다면?
+resize 되지않아도 바로 함수가 호출됨*/ 
+window.addEventListener("resize", handleResize);
+/*위와 같이 작성한다면 필요할 때(resize되었을 때)에 함수가 호출됨 */
