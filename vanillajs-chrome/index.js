731bf077efc22306db5275657b19e023144ec098
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-15 20:33:08 +0900

    Update index.js
    learned
    1. HTML can be changed(?) by JS
    2. querySelector

--- vanillajs-chrome/index.js
+++ vanillajs-chrome/index.js
@@ -1,5 +1,4 @@
-function sayHello(name, age){
-    return `Hello ${name} you are ${age} years old`;
-}
-const greetNicolas = sayHello("Nicolas",15);
-console.log(greetNicolas);
+const title = document.querySelector("#title");
+title.innerHTML="Hi! From JS";
+title.style.color = "red";
+document.title = 'I own you now';
