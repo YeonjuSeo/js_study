cc04d162c116ac17c4479e2dbb1f203ebca89fff
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-24 00:34:10 +0900

    Print Every Element of Array

--- javascript/function.js
+++ javascript/function.js
@@ -24,3 +24,12 @@ calculate("divide", 9, 3);
 calculate("multiply", 9, 3);
 calculate("remainder", 10, 3);
 calculate("malibu", 9, 3);
+
+const fruits = ["apple", "banana"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+fruits.forEach((fruit) => console.log(fruit));
